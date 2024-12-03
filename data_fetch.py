64e47455b52bb9907a6f4738930746f2385a50f0
Hamza Khan <63538732+hamza276@users.noreply.github.com>
2024-12-03 15:02:04 +0500

    data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -1,8 +1,8 @@
 # data_fetch.py
 import yfinance as yf
 
-def fetch_ticker_data(tickers, start="2010-01-01", end="2024-11-01"):
-    data = yf.download(tickers, start=start, end=end)['Adj Close']
+def fetch_ticker_data(tickers, start="2000-01-01", end="2024-11-01"):
+    data = yf.download(tickers, start=start, end=end)
         # Calculate summary statistics
     returns = data.pct_change().dropna()
     mean_returns = returns.mean().to_dict()
